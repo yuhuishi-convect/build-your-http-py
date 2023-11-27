f5c2c682ed667975de74f9e4d26813442c3298ae
Yuhui Shi <hi@yuhuishi.me>
2023-11-26 22:06:44 -0800

    read data

--- app/main.py
+++ app/main.py
@@ -7,6 +7,13 @@ def respond_200(client_socket):
     client_socket.send(response.encode())
 
 
+def read_request(client_socket):
+    buffer_size = 1024
+    request = client_socket.recv(buffer_size).decode()
+    print(request)
+    return request
+
+
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
@@ -15,6 +22,7 @@ def main():
     #
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     s, _ = server_socket.accept() # wait for client
+    read_request(s)
     respond_200(s)
 
 
